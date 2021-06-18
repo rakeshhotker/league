92d85225078da9e2815c2f70b604f6ab810338ea
varunsh17 <varunsh170502@gmail.com>
2021-06-18 22:13:44 +0530

    added grid on homepage

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import "./style.css"
-
-
+import pic1 from "./img/img4.jpg"
+import home2 from "./img/home2.mp4"
+import {Button,Container,Grid} from "@material-ui/core"
 function Download() {
     return (
         <div className="downloadall" id="download">
+        <>
+        <video poster={pic1} className='videoTag' autoPlay loop muted> 
+            <source src={home2} type='video/mp4' />
+            </video>
+        </>
+        <Button style={{position:"relative",top:"-30vh",width:"35vh",background:"#4FFBDF",color:"black"}}>PLAY NOW</Button>
             
         </div>
     )
 }
 
 export default Download
+
+        
+        
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,65 +2,102 @@ import React from 'react'
 import "./style.css";
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import {Link} from "react-scroll"
-import pic1 from "./img/img4.jpg"
-import home2 from "./img/home2.mp4"
+import { Grid ,Paper,Typography,makeStyles} from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    opacity:0.6,position:"relative",
+    top:"70px",
+    height:"70vh",
+    fontSize:"3vw",
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
     return (
+      
+
     <div className="homeall" id="home">
     <div className="nav">
-
-    
     <Link activeClass="active" className="link"
       to="home"
       spy={true}
       smooth={true}
       offset={0}
-      duration={500}
->
-  <HomeIcon
-    color="secondary"
-  /></Link>
-<Link activeClass="active" className="link"
-      to="download"
-      spy={true}
-      smooth={true}
-      offset={0}
-      duration={500}
->
-  <GetAppIcon
-    color="secondary"
-  />
-</Link>
-<Link activeClass="active" className="link"
-      to="login"
-      spy={true}
-      smooth={true}
-      offset={0}
-      duration={500}
->
-  <StorefrontIcon
-    color="secondary"
-  />
-</Link>
-<Link activeClass="active" className="link"
-      to="Contact"
-      spy={true}
-      smooth={true}
-      offset={0}
-      duration={500}
->
-  <QuestionAnswerIcon
-    color="secondary"
-  />
-</Link>
+      duration={800}
+      >
+        <HomeIcon
+          color="secondary"
+        /></Link>
+      <Link activeClass="active" className="link"
+            to="download"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
+      >
+        <GetAppIcon
+          color="secondary"
+        />
+      </Link>
+      <Link activeClass="active" className="link"
+            to="login"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
+      >
+        <StorefrontIcon
+          color="secondary"
+        />
+      </Link>
+      <Link activeClass="active" className="link"
+            to="Contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
+      >
+        <QuestionAnswerIcon
+          color="secondary"
+        />
+      </Link>
     </div>
-        <video poster={pic1} className='videoTag' autoPlay loop>
-            <source src={home2} type='video/mp4' />
-        </video>
+    <div className="container-home">
+    <Grid container spacing={2}>
+        
+        <Grid item lg={3} md={3} xs={3} sm={3} >
+          <Paper className={classes.paper}>
+          START YOUR JOURNEY NOW WITH WITH NEW WORLD </Paper>
+        </Grid>
+        <Grid item lg={3} md={3} xs={3} sm={3}>
+          <Paper className={classes.paper}>DOWNLOAD AND PLAY WITH CHARACTER YOU WANT AND GET 
+          BONUS</Paper>
+        </Grid>
+        <Grid item lg={3} md={3} xs={3} sm={3}>
+          <Paper className={classes.paper}>WE HAVE AKALI<br/>YASUO<br/>LUX<br/>JINX <br/>AND MANY MORE</Paper>
+        </Grid>
+        <Grid item lg={3} md={3} xs={3} sm={3}>
+          <Paper className={classes.paper}>FEEL THE WORLD LEAGUE <br/>2021 !</Paper>
+        </Grid>
+      </Grid>
+    </div>
+        
+
+
+
+
+
+
     </div>
 
     )
